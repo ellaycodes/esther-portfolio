d54777454df4cefe955f110a4e183444bd41cf76
Esther <11yekinie@gmail.com>
2025-04-14 10:32:48 +0100

    create api page

---
+++ estherPortfolio/src/api/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+/*const instance = axios.create({
+    method: 'get',
+    baseURL: 'https://cdn.contentful.com/api',
+    headers: {
+        'Authorization': `Bearer ${accessToken}`
+    }
+})
+
+console.log(instance);*/
